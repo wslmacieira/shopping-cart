b5357c06bca79f41856387bfb2b257e827b9ef0f
wslmacieiradev <wslmacieira@outlook.com>
2021-03-23 08:08:06 -0300

    Criando ProductService e Product model

--- src/app/components/shopping-cart/product-list/product-list.component.ts
+++ src/app/components/shopping-cart/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  productList: Product[] = [];
+
+  constructor(
+    private productService: ProductService
+  ) { }
 
   ngOnInit(): void {
+    this.productList = this.productService.getProducts();
   }
 
 }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  // TODO: Populate products from an API
+  products: Product[] = [
+    new Product(1, 'Product 1', 'This is  the product 1 description. The product is really kool!', 100),
+    new Product(2, 'Product 2', 'This is  the product 2 description. The product is really kool!', 150),
+    new Product(3, 'Product 3', 'This is  the product 3 description. The product is really kool!', 200),
+    new Product(4, 'Product 4', 'This is  the product 4 description. The product is really kool!', 300),
+    new Product(5, 'Product 5', 'This is  the product 5 description. The product is really kool!', 250),
+    new Product(6, 'Product 6', 'This is  the product 6 description. The product is really kool!', 100),
+    new Product(7, 'Product 7', 'This is  the product 7 description. The product is really kool!', 100),
+  ];
+
+  constructor() { }
+
+  getProducts(): Product[] {
+    // TODO: Populate products from an API and return an Observable
+    return this.products;
+  }
+}
